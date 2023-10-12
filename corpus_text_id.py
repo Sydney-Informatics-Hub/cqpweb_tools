9810c51f1c52c79e043d4f0e5ed913226c4db8b9
Mike Lynch <m.lynch@sydney.edu.au>
2023-10-12 16:45:34 +1100

    Improved ID remapping

--- corpus_text_id.py
+++ corpus_text_id.py
@@ -4,11 +4,12 @@
 
 from pathlib import Path
 import re
-import json
 import logging
 from openpyxl import load_workbook
+import sys
 
 LOGLEVEL = logging.INFO
+LOGDIR = "./logs"
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +17,10 @@ logger.setLevel(LOGLEVEL)
 logch = logging.StreamHandler()
 logch.setLevel(LOGLEVEL)
 logger.addHandler(logch)
+logfh = logging.FileHandler(Path(LOGDIR) / "corpus_text_id.log")
+logfh.setLevel(LOGLEVEL)
+logger.addHandler(logfh)
+
 
 
 CORPUS_ORIG = "corpus_orig"
@@ -23,10 +28,10 @@ CORPUS_REMAPPED = "corpus_remapped"
 
 METADATA_EXCEL = "List of treaties_CQWEBMetadata_UPDATED2023.xlsx"
 
-COL_NEW_ID = 1 #'B'
+COL_NEW_ID = 1 # 'B'
 COL_OLD_ID = 6 # 'G'
-TEXT_ID_RE = re.compile(r'<text id="([^\"]*)">')
-TEXT_ID_TEMPLATE = '<text id="{id}">'
+TRUNCATE_TO = 40
+TEXT_ID_RE = re.compile(r'(<text id=")([^\"]*)(">.*)')
 
 def load_id_mappings(excelfile):
     wb = load_workbook(excelfile)
@@ -35,20 +40,27 @@ def load_id_mappings(excelfile):
     for row in ws.iter_rows():
         new_id = row[COL_NEW_ID].value
         old_id = row[COL_OLD_ID].value
-        if new_id and new_id != 'Text ID':
-            mappings[old_id] = new_id
+        if not new_id is None and not old_id is None:
+            if new_id != 'Text ID':
+                if TRUNCATE_TO:
+                    old_id = old_id[:TRUNCATE_TO]
+                mappings[old_id] = new_id
+                logger.info(f"map old {old_id} new {new_id}")
     return mappings
 
 def convert_ids(mappings, cfh):
     for line in cfh:
         if m := TEXT_ID_RE.match(line):
-            old_id = m.group(1)
+            prefix = m.group(1)
+            old_id = m.group(2)
+            suffix = m.group(3)
             if not old_id in mappings:
                 logger.warn(f"Warning: id {old_id} in {corpus_file} not in spreadsheet")
                 yield line
             else:
                 new_id = mappings[old_id]
-                yield TEXT_ID_TEMPLATE.format(id=new_id)
+                new_line = f"{prefix}{new_id}{suffix}\n"
+                yield new_line
         else:
             yield line
 
@@ -59,12 +71,28 @@ def convert_file(mappings, orig_file, new_file):
             for newline in convert_ids(mappings, ofh):
                 nfh.write(newline)
 
+def check_ids(mappings, orig_file):
+    n = 0;
+    print(orig_file)
+    old_ids = mappings.keys()
+    new_ids = mappings.values()
+    with open(orig_file, "r") as ofh:
+        for line in ofh:
+            if m := TEXT_ID_RE.match(line):
+                old_id = m.group(2)
+                found_old = old_id in old_ids
+                found_new = old_id in new_ids
+                print(f"{n},{old_id},{found_old},{found_new}")
+            n += 1
+
 
 mappings = load_id_mappings(METADATA_EXCEL)
 
-print(json.dumps(mappings, indent=2))
+files = list(Path(CORPUS_ORIG).glob("war*"))
+files.sort()
 
-for corpus_file in Path(CORPUS_ORIG).glob("war*"):
+for corpus_file in files:
+    check_ids(mappings, corpus_file)
     new_file = Path(CORPUS_REMAPPED) / corpus_file.name
     logger.info(f"{corpus_file} => {new_file}")
     convert_file(mappings, corpus_file, new_file)
