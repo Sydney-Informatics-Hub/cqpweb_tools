fa92a3bb0f76f95c64e83112b751a991aa391017
Mike Lynch <m.lynch@sydney.edu.au>
2023-12-21 16:17:00 +1100

    Modified the corpus_rewrite.py script so that it writes out the corpus texts with a file for each text

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # cqpweb-utils
 
 Scripts for wrangling corpora and metadata files for CQPweb.
+
+Writing some notes down so that I can remember what I was doing with this.
+
+Problems which the reindex is supposed to fix:
+
+* update the text IDs to match the 2023 spreadsheet
+* make sure that the metadata .tsv matches values in the 2023 spreadsheet
+* fix the ordering problem - CQPweb returns search results in corpus order -
+  this is the order in which the corpus was indexed, by filename and then
+  by order within the corpus. At first, I thought this only affected one
+  text, 1949_2_Second_Geneva_Convention_WoundedS - which is at the end of the
+  last corpus file. However, when checking the text ids, I found that there
+  are ten or more such texts (the order in which they appear in the current
+  corpus is different to the order in the spreadsheet)
+
+
+I've already written a script which rewrites out the corpus with updated text
+ids, this is corpus_text_id.py - its inputs are the 2023 spreadsheet.
+
+This needs to be adapted so that it writes out the corpus in the order given
+by the spreadsheet. At present, it makes a copy of the corpus in the same order
+which it was originally found.
+
+The current version uses a line-by-line approach and copies the filenames. The
+new version needs to 
+
+- read a textID and find the new mapped ID if one exisst
+- either collect the rest of the text or write it out somewhere
+
+A simple approach: write out each of the texts as a separate file with a 
+filename giving the sequence (ie 1856_Paris_DecRespectingMaritimeLaw written
+out as warlaw_001, etc)
+
+Then reassemble these into bigger chunks which preserve the order.
+
--- corpus_rewrite.py
+++ corpus_rewrite.py
@@ -21,15 +21,13 @@ logfh = logging.FileHandler(Path(LOGDIR) / "corpus_text_id.log")
 logfh.setLevel(LOGLEVEL)
 logger.addHandler(logfh)
 
-
-
 CORPUS_ORIG = "corpus_orig"
-CORPUS_REMAPPED = "corpus_remapped"
+CORPUS_REMAPPED = "corpus_test"
 
-METADATA_EXCEL = "List of treaties_CQWEBMetadata_UPDATED2023.xlsx"
+METADATA_EXCEL = "data/warlaw_metadata_2023.xlsx"
 
 COL_NEW_ID = 1 # 'B'
-COL_OLD_ID = 6 # 'G'
+COL_OLD_ID = 8 # 'I'
 TRUNCATE_TO = 40
 TEXT_ID_RE = re.compile(r'(<text id=")([^\"]*)(">.*)')
 
@@ -37,15 +35,18 @@ def load_id_mappings(excelfile):
     wb = load_workbook(excelfile)
     ws = wb.active
     mappings = {}
+    n = 1
     for row in ws.iter_rows():
         new_id = row[COL_NEW_ID].value
         old_id = row[COL_OLD_ID].value
         if not new_id is None and not old_id is None:
             if new_id != 'Text ID':
                 if TRUNCATE_TO:
-                    old_id = old_id[:TRUNCATE_TO]
-                mappings[old_id] = new_id
-                logger.info(f"map old {old_id} new {new_id}")
+                    if len(old_id) > TRUNCATE_TO:
+                        logger.warn(f"Corpus id {old_id} is > 40 chars")
+                mappings[old_id] = ( new_id, n )
+                logger.info(f"map old {old_id} to new {new_id} {n}")
+                n += 1
     return mappings
 
 def convert_ids(mappings, cfh):
@@ -63,6 +64,8 @@ def convert_ids(mappings, cfh):
                 yield new_line
         else:
             yield line
+
+
 
 
 def convert_file(mappings, orig_file, new_file):
@@ -86,13 +89,45 @@ def check_ids(mappings, orig_file):
             n += 1
 
 
+def write_temp_text(target_dir, n, new_id, content):
+    new_file = target_dir / Path(f"warlaw_{n:03d}_{new_id}")
+    with open(new_file, "w") as nfh:
+        nfh.write(content)
+    logger.info(f"Wrote {new_file}")
+
+
+
 mappings = load_id_mappings(METADATA_EXCEL)
 
 files = list(Path(CORPUS_ORIG).glob("war*"))
 files.sort()
 
+target_dir = Path(CORPUS_REMAPPED)
+target_dir.mkdir(parents=True, exist_ok=True)
+
 for corpus_file in files:
-    check_ids(mappings, corpus_file)
-    new_file = Path(CORPUS_REMAPPED) / corpus_file.name
-    logger.info(f"{corpus_file} => {new_file}")
-    convert_file(mappings, corpus_file, new_file)
+    contents = ""
+    new_id = None
+    n = None
+    with open(corpus_file) as ofh:
+        for line in ofh:
+            if m := TEXT_ID_RE.match(line):
+                prefix = m.group(1)
+                old_id = m.group(2)
+                suffix = m.group(3)
+                if not old_id in mappings:
+                    logger.error(f"Warning: id {old_id} in {corpus_file} not in spreadsheet")
+                    sys.exit(-1)
+                if new_id is not None:
+                    write_temp_text(target_dir, n, new_id, content)
+                ( new_id, n ) = mappings[old_id]
+                content = f"{prefix}{new_id}{suffix}\n"
+            else:
+                content += line
+    if new_id is not None:
+        write_temp_text(target_dir, n, new_id, content)
+
+
+#    new_file = target_dir / corpus_file.name
+    # logger.info(f"{corpus_file} => {new_file}")
+    # convert_file(mappings, corpus_file, new_file)
