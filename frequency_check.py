1ec5a0c55cf3bdfb7126a91b329aebb8bb09e1ab
Mike Lynch <m.lynch@sydney.edu.au>
2024-02-20 13:50:20 +1100

    Improved feedback from corpus tester script

--- frequency_check.py
+++ frequency_check.py
@@ -1,8 +1,8 @@
 # compare frequency lists
 
 
-PROD_FREQ = "frequency_prod.txt"
-DEV_FREQ = "frequency_list_dev6.txt"
+PROD_FREQ = "testing/frequency_prod_20240220.txt"
+DEV_FREQ = "testing/frequency_dev_20240220.txt"
 
 
 def load_freq_list(filename):
@@ -19,6 +19,7 @@ def load_freq_list(filename):
 f_prod = load_freq_list(PROD_FREQ)
 f_dev = load_freq_list(DEV_FREQ)
 
+print("Checking all tokens from prod...")
 for token, count in f_prod.items():
     if token not in f_dev:
         print(f"Not found in dev: {token}")
@@ -27,6 +28,10 @@ for token, count in f_prod.items():
             print(f"Count out for token {token} {f_dev[token]}")
         f_dev.pop(token)
 
+print("Done.")
+
 if len(f_dev) > 0:
     print("Tokens in dev not found in prod:")
     print(f_dev)
+else:
+    print("No leftover words in dev.")
